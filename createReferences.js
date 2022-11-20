80d7ee3c0a572325a7c30c1ddb58d522531f9ef8
JustinHodge <justinhodge8@gmail.com>
2022-11-20 14:25:36 -0500

    fix references structure

--- createReferences.js
+++ createReferences.js
@@ -10,35 +10,36 @@ import {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const createReferences = () => {
-    const references = {};
+    const references = { books: {} };
     const versions = getSupportedVersions();
-
     versions.forEach((version) => {
         const json = getJsonFromFile(`./${version}.json`);
 
         Object.values(json).forEach(
             ({ chapter, verse, text, translation_id, book_id, book_name }) => {
-                if (!references[book_id]) {
-                    references[book_id] = {
+                if (!references.books[book_id]) {
+                    references.books[book_id] = {
                         bookName: book_name,
                         bookId: book_id,
                         chapters: {},
                     };
                 }
 
-                if (!references[book_id].chapters[chapter]) {
-                    references[book_id].chapters[chapter] = {
+                if (!references.books[book_id].chapters[chapter]) {
+                    references.books[book_id].chapters[chapter] = {
                         chapterNumber: chapter,
                         verses: [],
                     };
                 }
 
                 if (
-                    !references[book_id].chapters[chapter].verses.some(
+                    !references.books[book_id].chapters[chapter].verses.some(
                         (seenVerse) => seenVerse === verse
                     )
                 ) {
-                    references[book_id].chapters[chapter].verses.push(verse);
+                    references.books[book_id].chapters[chapter].verses.push(
+                        verse
+                    );
                 }
             }
         );
